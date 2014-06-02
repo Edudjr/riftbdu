bda656fe9eb4ea7ad37b1e0b7a755855fd94fe83
jbrmaiden <jordanboaz14@gmail.com>
2014-06-02 12:50:48 -0400

    Rename of the Planet variable to 'Pointing' variable, to make it easier to understand

--- Unity/Assets/Scripts/Rotation.js
+++ Unity/Assets/Scripts/Rotation.js
@@ -1,13 +1,13 @@
 ﻿#pragma strict
 var speed : float = 50;
-var Planet : Transform;
+var Pointing : Transform;
 var PlanetName : String;
-Planet = GameObject.Find(PlanetName).transform;
+Pointing = GameObject.Find(PlanetName).transform;
 function Start () {
 
 }
 
 function Update () {
-transform.RotateAround(Planet.position, Vector3.up, speed*Time.deltaTime);
+transform.RotateAround(Pointing.position, Vector3.up, speed*Time.deltaTime);
 
 }
