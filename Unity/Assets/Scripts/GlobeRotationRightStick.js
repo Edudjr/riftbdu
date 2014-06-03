b7a65b3b28b5da808c44967e2f1bc5e394beea53
Edudjr <eduardo.djr@hotmail.com>
2014-06-03 08:31:14 -0400

    added earth's movement script and general modifications

---
+++ Unity/Assets/Scripts/GlobeRotationRightStick.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+public var speed : float = 1;
+public var referenceObject: String;
+public var rotationLimit = 3;
+
+private var reference : GameObject;
+
+function Start(){
+	reference = GameObject.Find(referenceObject);
+}
+function Update () {
+	//print(" X = " + transform.localRotation.x);
+	//print(" Y = " + transform.localRotation.y);
+	//print(" Z = " + transform.localRotation.z);
+
+	getAxis(null);
+}
+
+function getAxis (input : String){
+
+	
+	if((Input.GetAxisRaw("RightAnalog_Horizontal")> 0.3)||(input == "right"))
+	{ 
+		transform.Rotate(-Vector3.up * Time.deltaTime * speed, Space.Self);
+	}
+	else {
+		if ((Input.GetAxisRaw("RightAnalog_Horizontal") < -0.3)||(input == "left"))
+		{
+			transform.Rotate(Vector3.up * Time.deltaTime * speed, Space.Self);
+		}
+	}
+	
+	if((Input.GetAxisRaw("RightAnalog_Vertical")> 0.3)||(input == "up"))
+	{
+		if(reference.transform.position.z > transform.position.z - rotationLimit)
+		//if(reference.transform.position.z > -1.2)
+			transform.Rotate(-Vector3.right * Time.deltaTime * speed, Space.World);
+	}
+	else{
+		if((Input.GetAxisRaw("RightAnalog_Vertical") < -0.3)||(input == "down"))
+		{
+			if(reference.transform.position.z < transform.position.z + rotationLimit)
+			//if(reference.transform.position.z < 1.3)
+				transform.Rotate(Vector3.right * Time.deltaTime * speed, Space.World);
+		}
+	}
+}
--- Unity/Assets/Scripts/Rotation.js
+++ Unity/Assets/Scripts/Rotation.js
@@ -1,13 +1,16 @@
 ﻿#pragma strict
-var speed : float = 50;
+
 private var Planet : Transform;
-var PlanetName : String;
-Pointing = GameObject.Find(PlanetName).transform;
+var speed : float = 50;
+var RotateAround : String;
+
+Planet = GameObject.Find(RotateAround).transform;
+
 function Start () {
 
 }
 
 function Update () {
-transform.RotateAround(Pointing.position, Vector3.up, speed*Time.deltaTime);
-
+	if(Planet != null)
+		transform.RotateAround(Planet.position, Vector3.up, speed*Time.deltaTime);
 }
