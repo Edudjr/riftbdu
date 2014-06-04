3a693caad328c7c72edb3f1909428e977cf43adb
Edudjr <eduardo.djr@hotmail.com>
2014-06-04 15:32:12 -0400

    fixed some bugs

---
+++ Unity/Assets/Scripts/Markers.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+private var Globe : GameObject;
+private var index : int;
+private var pos : int = 0;
+private var matt : Material;
+
+function Awake ()
+{
+	Globe = GameObject.Find("Sphere");
+}
+function Update(){
+print("PosX: "+transform.position.x);
+print("PosY: "+transform.position.y);
+print("PosZ: "+transform.position.z);
+
+if(transform.position.z < 0){	
+	if((transform.position.y < 0.4)&&(transform.position.y > -0.4)){
+		if((transform.position.x < 0.4)&&(transform.position.x > -0.4)){
+			
+			for(index = 0; index<Globe.renderer.materials.length; index++){
+				matt = Globe.renderer.materials[index];
+				if (matt.name == (transform.name + " (Instance)")){
+					matt.color.a = 90;
+					pos = index;
+					//GameObject.Find("Timer").SendMessage("GetMarker", transform.name);
+					//GameObject.Find("Timer").SendMessage("newGetMarker", transform.name);
+				}
+			}
+		}
+		else
+			Globe.renderer.materials[pos].color.a = 0;
+	}
+	else 
+		Globe.renderer.materials[pos].color.a = 0;
+}
+	
+}
+
+function OnDestroy ()
+{
+	DestroyImmediate(renderer.material);
+}
---
+++ Unity/Assets/Scripts/Materials.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Awake ()
+{
+	var i : int;
+	
+	for (i = 1; i < renderer.materials.length; i++){
+		renderer.materials[i].color.a = 0;
+	}
+}
+function Update(){
+
+}
+
+function OnDestroy ()
+{
+	DestroyImmediate(renderer.material);
+}
