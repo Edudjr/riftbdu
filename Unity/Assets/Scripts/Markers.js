e2e7e8693617785c102d872b916ddaf0d61fe817
Edudjr <eduardo.djr@hotmail.com>
2014-06-13 15:01:43 -0400

    changed markers script

--- Unity/Assets/Scripts/Markers.js
+++ Unity/Assets/Scripts/Markers.js
@@ -4,6 +4,7 @@ private var Globe : GameObject;
 private var index : int;
 private var pos : int = 0;
 private var matt : Material;
+public var precision : float = 0.4;
 
 function Awake ()
 {
@@ -14,24 +15,25 @@ function Update(){
 //print("PosY: "+transform.position.y);
 //print("PosZ: "+transform.position.z);
 
-if(transform.position.z < 0){	
-	if((transform.position.y < 0.4)&&(transform.position.y > -0.4)){
-		if((transform.position.x < 0.4)&&(transform.position.x > -0.4)){
-			
-			for(index = 0; index<Globe.renderer.materials.length; index++){
-				matt = Globe.renderer.materials[index];
-				if (matt.name == (transform.name + " (Instance)")){
-					matt.color.a = 90;
-					pos = index;
-					//GameObject.Find("Timer").SendMessage("GetMarker", transform.name);
-					//GameObject.Find("Timer").SendMessage("newGetMarker", transform.name);
+	if(transform.position.z < 0){	
+		if((transform.position.y < precision)&&(transform.position.y > -precision)){
+			if((transform.position.x < precision)&&(transform.position.x > -precision)){
+				
+				for(index = 0; index<Globe.renderer.materials.length; index++){
+					matt = Globe.renderer.materials[index];
+					if (matt.name == (transform.name + " (Instance)")){
+						//Debug.Log(transform.position);
+						matt.color.a = 90;
+						pos = index;
+						//GameObject.Find("Timer").SendMessage("GetMarker", transform.name);
+						//GameObject.Find("Timer").SendMessage("newGetMarker", transform.name);
+					}
 				}
 			}
+			else
+				Globe.renderer.materials[pos].color.a = 0;
 		}
-		else
-			Globe.renderer.materials[pos].color.a = 0;
-	}
-	else 
+		else 
 		Globe.renderer.materials[pos].color.a = 0;
 }
 	
