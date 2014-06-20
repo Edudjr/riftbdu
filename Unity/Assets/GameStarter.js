5b605bb499332ee570d12f3f2d698d63bd947433
jbrmaiden <jordanboaz14@gmail.com>
2014-06-20 13:30:50 -0400

    Created a script to control the game

---
+++ Unity/Assets/GameStarter.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+public var referenceObject: String;
+private var ObjectStarter : GameObject;
+
+
+function Start () {
+	ObjectStarter = GameObject.Find(referenceObject);
+}
+
+function Update () {
+
+}
